1d90de8cfbdb4586f868a9220a6ce2b0cd5daa46
So Hoan Hua <huasohoan@gmail.com>
2023-03-02 22:20:02 -0800

    Feature 6 complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from receipts.views import receipt_list
 
-urlpatterns = [path("receipts/", receipt_list, name="receipt_list")]
+urlpatterns = [
+    path("", receipt_list, name="home"),
+]
 # if i wanna put this url into a link, name goes into {% url name %}
 # then django will go from main url => app's url
